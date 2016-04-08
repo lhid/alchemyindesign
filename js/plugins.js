1913458990b853b1f998365ba125610aec74d3e7
Leban Hyde <leban@alchemyindesign.com>
2016-04-08 05:55:46 -0500

    Moved contact.js to plugins.js
    Moved form handling script to plugins.js to minimize assets delivered
    and cleaner directories.

--- js/plugins.js
+++ js/plugins.js
@@ -29,6 +29,37 @@
 
 
 /**
+ * Form handling. Authored by Ryan Deba.
+ */
+
+$(document).ready(function(){
+  $("#contact-form").bind('submit',function(){
+    //let's start by disabling the submit button. hopefully this will help prevent any issues with people double-clicking and submitting the form multiple times
+    $("#send").attr("disabled","disabled");
+
+    //ajax post request
+    $.post(
+      "contact.php",
+      $("#contact-form").serialize(),
+      function(data){
+        if (data == 'true'){
+          //success
+          $("#send").attr("value","Message Sent");
+        } else {
+          //failure
+          //not sure what to do here...
+          $("#send")
+            .attr("value","Send Failed. Try Again?")
+            .removeAttr("disabled");
+        }
+      });
+    return false;
+  });
+});
+
+
+
+/**
  * smooth-scroll v5.3.7
  * Animate scrolling to anchor links, by Chris Ferdinandi.
  * http://github.com/cferdinandi/smooth-scroll
